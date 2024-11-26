bd882825faa4baf4a5f06c27ba9d0142c393c73d
dot123 <conjurer888888@gmail.com>
2024-11-26 17:01:04 +0800

    重命名

--- internal/middleware/rateLimiter.go
+++ internal/middleware/rateLimiter.go
@@ -41,11 +41,11 @@ func RateLimiterMiddleware(skippers ...SkipperFunc) gin.HandlerFunc {
 		userID := contextx.FromUserID(c.Request.Context())
 		if userID != 0 {
 			limit := cfg.Count
-			rate, delay, allowed := limiter.AllowMinute(fmt.Sprintf("%d", userID), limit)
+			count, delay, allowed := limiter.AllowMinute(fmt.Sprintf("%d", userID), limit)
 			if !allowed {
 				h := c.Writer.Header()
 				h.Set("X-RateLimit-Limit", strconv.FormatInt(limit, 10))
-				h.Set("X-RateLimit-Remaining", strconv.FormatInt(limit-rate, 10))
+				h.Set("X-RateLimit-Remaining", strconv.FormatInt(limit-count, 10))
 				delaySec := int64(delay / time.Second)
 				h.Set("X-RateLimit-Delay", strconv.FormatInt(delaySec, 10))
 				ginx.ResError(c, errors.ErrTooManyRequests)
